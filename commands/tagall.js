cd86aa4fa00ab49003b062bddcc7f2a996589d24
sukuna0567 <josephharrysonmarc@gmail.com>
2025-04-06 04:42:31 -0400

    Update tagall.js

--- commands/tagall.js
+++ commands/tagall.js
@@ -1,1 +1,18 @@
+module.exports = async (client, msg) => {
+    const chat = await msg.getChat();
 
+    if (!chat.isGroup) {
+        return msg.reply("This command only works in groups.");
+    }
+
+    let text = '*Tagging everyone:* \n\n';
+    let mentions = [];
+
+    for (let participant of chat.participants) {
+        const contact = await client.getContactById(participant.id._serialized);
+        mentions.push(contact);
+        text += `@${contact.id.user} `;
+    }
+
+    await chat.sendMessage(text, { mentions });
+};
