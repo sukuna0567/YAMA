77cf2a82315ed9d8fedbc2e84ad27d0168239e3e
sukuna0567 <josephharrysonmarc@gmail.com>
2025-04-06 08:34:48 -0400

    Create codeGenerator.js

---
+++ utils/codeGenerator.js
@@ -0,0 +1,10 @@
+function generateCode(length = 6) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let code = ''
+  for (let i = 0; i < length; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+  return code
+}
+
+module.exports = generateCode
