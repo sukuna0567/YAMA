bc8b44b3bbab3f8fffc1b1ec8b87a2398ca03f36
sukuna0567 <josephharrysonmarc@gmail.com>
2025-04-06 05:00:40 -0400

    Update timeGreeting.js

--- utils/timeGreeting.js
+++ utils/timeGreeting.js
@@ -1,1 +1,6 @@
-
+module.exports = () => {
+    const hour = new Date().getHours();
+    if (hour < 12) return "Good morning";
+    if (hour < 18) return "Good afternoon";
+    return "Good evening";
+};
